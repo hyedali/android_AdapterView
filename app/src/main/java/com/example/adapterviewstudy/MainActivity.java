0e408dade8e9e1558cabc7051628c75abf4c2709
hyedali <ekek9654@gmail.com>
2022-03-18 23:33:52 +0900

    first commit

---
+++ app/src/main/java/com/example/adapterviewstudy/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.adapterviewstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckedTextView;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    final ArrayList<String> al = new ArrayList<String>();
+    private ListView listView;
+    private Button add_btn, del_btn;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_checked, al);
+
+        listView = findViewById(R.id.list_item);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                CheckedTextView check = (CheckedTextView)view;
+                check.setChecked(!check.isChecked());
+            }
+        });
+
+        editText = findViewById(R.id.text);
+        add_btn = findViewById(R.id.add);
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                al.add(editText.getText().toString());
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
